0a92015352a19f801468f5221cc2af66d847da24
David Spano <david.spano@move.com>
2020-04-21 17:02:55 -0400

    initial commit to react tutorial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi, I am a react App</h1>
+      <p>I think I like React!</p>
+    </div>
+  );
+  // return React.createElement('div', null, React.createElement('h1', {className: 'App'},  'Hi, I\'m a react App!!!'));
+}
+
+export default App;
