f8db26bd2c403e30b9a9df0a7a4e6c3a37daa3ed
David Spano <david.spano@move.com>
2020-04-22 07:28:34 -0400

    react hook example with useState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Person from './Person/person';
 
-function App() {
+const App = props => {
+  // returns array with exactly 2 elements  currentState and function to update state
+  const [ personsState, setPersonsState ] = useState({
+    persons: [
+      { name: "David", age: 37 },
+      { name: "Haley", age: 30 },
+      { name: "Chloe", age: 1 }
+    ]
+    
+  });
+
+  const [otherState, setOtherState] = useState({otherState: 'some other value'});
+
+  const switchNameHandler = () => {
+    setPersonsState({
+      persons: [
+        { name: "Big Daddy Dave", age: 37 },
+        { name: "Haley", age: 30 },
+        { name: "Chloe", age: 1 }
+      ],
+      otherState: personsState.otherState
+    });
+  };
+
+  console.log(personsState, otherState);
   return (
     <div className="App">
       <h1>Hi, I am a react App</h1>
-      <p>I think I like React!</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person age={personsState.persons[0].age} name={personsState.persons[0].name}> I like beer </Person>
+      <Person age={personsState.persons[1].age} name={personsState.persons[1].name}> I like wine </Person>
+      <Person age={personsState.persons[2].age} name={personsState.persons[2].name}> I like milk </Person>
     </div>
   );
   // return React.createElement('div', null, React.createElement('h1', {className: 'App'},  'Hi, I\'m a react App!!!'));
 }
 
+
 export default App;
+
+// state = {
+//   persons: [
+//     { name: "David", age: 37 },
+//     { name: "Haley", age: 30 },
+//     { name: "Chloe", age: 1 }
+//   ]
+// }
+
+// switchNameHandler = () => {
+//   this.setState({ persons: [
+//     { name: "Big Daddy Dave", age: 37 },
+//     { name: "Haley", age: 30 },
+//     { name: "Chloe", age: 1 }
+//   ]});
+// }
